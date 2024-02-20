e7248b36cf8638d0bb5a5053044e939ee831c7e6
Werle-G <g.werle@laposte.net>
2024-02-20 09:38:06 +0100

    VichUploaderBundle

--- src/Form/ImageConcertType.php
+++ src/Form/ImageConcertType.php
@@ -2,13 +2,12 @@
 
 namespace App\Form;
 
-use App\Entity\Concert;
+// use App\Entity\Concert;
 use App\Entity\ImageConcert;
-use App\Form\ImageConcertType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
@@ -17,8 +16,12 @@ class ImageConcertType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nomImage')
-            ->add('alt')
+            ->add('nomImage', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('alt', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
             // ->add('concert', EntityType::class, [
             //     'class' => Concert::class,
             //     'choice_label' => 'id',
